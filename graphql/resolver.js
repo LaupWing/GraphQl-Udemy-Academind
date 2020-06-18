533730cd835af5bc5418acf2f9c889471b9586f0
LaupWing <loc_strict@hotmail.com>
2020-06-18 22:36:25 +0200

    435

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -106,16 +106,22 @@ module.exports = {
             updatedAt: createdPost.updatedAt.toISOString()
         }
     },
-    async posts(args, req){
+    async posts({page}, req){
         if(!req.isAuth){
             const error = new Error('Not Authenitcated');
             error.code = 401;
             throw error;
         }
+        if(!page){
+            page = 1;
+        }
+        const perPage = 2;
         const totalPosts = await Post.find().countDocuments();
         const posts = await Post
             .find()
             .sort({createdAt: -1})
+            .skip((page-1) * perPage)
+            .limit(perPage)
             .popupalate('creator');
         
         return {posts: posts.map(x=> (
