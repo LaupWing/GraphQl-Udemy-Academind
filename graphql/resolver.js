f6e06ffee10a91daa82c3f177db322e472969286
LaupWing <loc_strict@hotmail.com>
2020-06-19 15:42:48 +0200

    439

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -4,6 +4,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const Post = require('../models/post');
 const post = require('../models/post');
+const { updatePost } = require('../controllers/feed');
 
 module.exports = {
     createUser: async function({userInput}, req){
@@ -140,7 +141,7 @@ module.exports = {
             error.code = 401;
             throw error;
         }
-        const post = await (await Post.findById(id)).populated('creator');
+        const post = await Post.findById(id).populate('creator');
         if(!post){
             const error = new Error('Post not found');
             error.code = 404;
@@ -152,5 +153,54 @@ module.exports = {
             createdAt: post.createdAt.toISOString(),
             updatedAt: post.updatedAt.toISOString(),
         }
+    },
+    async updatePost({id, postInput}, req){
+        if(!req.isAuth){
+            const error = new Error('Not Authenitcated');
+            error.code = 401;
+            throw error;
+        }
+        const post = await Post.findById(id).populate('creator');
+        if(!post){
+            const error = new Error('Post not found');
+            error.code = 404;
+            throw error;
+        }
+        if(post.creator._id.toString() !== req.userId.toString()){
+            const error = new Error('Not Authroized');
+            error.code = 403;
+            throw error;
+        }
+        
+        const errors = [];
+        if(validator.isEmpty(postInput.title) || !validator.isLength(postInput.title, {min:5})){
+            errors.push({
+                message: 'Title is invalid'
+            });
+        }
+        if(validator.isEmpty(postInput.content) || !validator.isLength(postInput.content, {min:5})){
+            errors.push({
+                message: 'Content is invalid'
+            });
+        }
+        if(errors.length>0){
+            const error = new Error('Invalid input');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+
+        post.title = postInput.title;
+        post.content = postInput.content;
+        if(postInput.imageUrl !== 'undefined'){
+            post.imageUrl = postInput.imageUrl;
+        }
+        const updatedPost = await post.save();
+        return{
+            ...updatedPost._doc,
+            _id: updatedPost._id.toString(),
+            createdAt: updatedPost.createdAt.toISOString(),
+            updatedAt: updatedPost.updatedAt.toISOString(),
+        };
     }
 }
